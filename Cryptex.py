8ce0f814b34254fd2ababa6cb94375fdfa27dd2b
FocusWaiver <113658378+FocusWaiver@users.noreply.github.com>
2026-08-25 14:30:16 -0400

    Update Cryptex.py
    target = "output"

--- Cryptex.py
+++ Cryptex.py
@@ -6,6 +6,6 @@ def AnswerCk(event):
     input_text = web.page["guess"]
     guess = input_text.value
     if guess == "guess":
-        display("Correct!", target= "output-div")
+        display("Correct!", target= "output")
     else:
-        display("Nope, Try Again", target= "output-div")
+        display("Nope, Try Again", target= "output")
