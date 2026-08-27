ef45ec04c34c193da35be964ce6be14c9cfd8cc3
FocusWaiver <113658378+FocusWaiver@users.noreply.github.com>
2026-08-27 18:42:03 -0400

    Update Cryptex.py
    verify input is 5 letters

--- Cryptex.py
+++ Cryptex.py
@@ -4,8 +4,23 @@ from pyscript import web, when, display
 @when("click", "#guess-button")
 def AnswerCk(event):
     input_text = web.page["guess"]
-    guess = input_text.value
-    if guess == "guess":
-        display("Correct!", target= "output")
+    guessRAW = input_text.value
+    guess = guessRAW.lower()
+    if Valid(guess):
+        if guess == "guess":
+            display("Correct!", target= "output")
+        else:
+            display("Nope, Try Again", target= "output")
     else:
-        display("Nope, Try Again", target= "output")
+        display("Sorry, your guess must be 5 letters.")
+def Valid(ans):
+    anslist = list(ans)
+    Vkey = "abcdefghijklmnopqrstuvwxyz"
+    if len(ans) != 5:
+        return False
+    else:
+        #check against valid characters
+        for i in anslist:
+            if Vkey.find(i) < 0:
+                return False
+        return True
